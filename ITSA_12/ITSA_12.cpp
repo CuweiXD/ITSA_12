a5936dc3dc6e70ab4727e04b7df4801661dbc5cf
勝崴 施 <4b1g0178@office.stust.edu.tw>
2023-03-18 08:39:02 +0800

    加入專案檔案。

---
+++ ITSA_12/ITSA_12.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int f(int(x))
+{
+
+    if (x == 0 || x == 1) {
+
+        return x + 1;
+    }
+
+    else if (x > 1) {
+        return f(x - 1) + f(x / 2);
+    }
+
+}
+int main()
+{
+    int x;
+    cin >> x;
+    cout << f(x) << endl;
+}
+
